cda6fa631e2ad96489259f8887aaaccbb6c61be2
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2026-05-31 03:18:32 +0900

    feat: 앱 복귀 시 날짜 갱신 추가

--- apps/mobile/lib/app/app.dart
+++ apps/mobile/lib/app/app.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'router.dart';
+import '../core/date/today_controller.dart';
 import '../core/theme/app_theme.dart';
+import 'router.dart';
 
-class VinscentApp extends ConsumerWidget {
+class VinscentApp extends ConsumerStatefulWidget {
   const VinscentApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<VinscentApp> createState() => _VinscentAppState();
+}
+
+class _VinscentAppState extends ConsumerState<VinscentApp>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      ref.read(todayControllerProvider.notifier).refresh();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final router = ref.watch(appRouterProvider);
 
     return MaterialApp.router(
