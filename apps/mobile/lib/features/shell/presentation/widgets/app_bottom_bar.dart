36371f97d9930f44c2a58575b52fc9559b64f2d5
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2026-07-17 04:13:29 +0900

    fix: 플로팅 독 하단 여백 확대

--- apps/mobile/lib/features/shell/presentation/widgets/app_bottom_bar.dart
+++ apps/mobile/lib/features/shell/presentation/widgets/app_bottom_bar.dart
@@ -31,7 +31,7 @@ class AppBottomBar extends StatelessWidget {
       height: height + bottomInset,
       width: double.infinity,
       child: Padding(
-        padding: EdgeInsets.fromLTRB(16, 8, 16, 16 + bottomInset),
+        padding: EdgeInsets.fromLTRB(16, 8, 16, 24 + bottomInset),
         child: DecoratedBox(
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(_surfaceRadius),
