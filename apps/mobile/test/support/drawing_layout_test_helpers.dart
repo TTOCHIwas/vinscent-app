e4b28ba49c079b29818f75ed9522607cad3e1460
TTOCHIwas <95687307+TTOCHIwas@users.noreply.github.com>
2026-09-05 07:45:22 +0900

    test: 그림 도구 하단 배치와 밝은 배경 검증

--- apps/mobile/test/support/drawing_layout_test_helpers.dart
+++ apps/mobile/test/support/drawing_layout_test_helpers.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:vinscent/core/drawing/widgets/app_drawing_width_slider.dart';
+import 'package:vinscent/core/drawing/widgets/app_drawing_style_controls.dart';
+import 'package:vinscent/core/presentation/widgets/app_page_header.dart';
+import 'package:vinscent/core/theme/app_colors.dart';
 
 void expectSharedDrawingLayout(
   WidgetTester tester, {
   required String keyPrefix,
   required Finder canvas,
 }) {
-  final top = tester.getRect(find.byKey(ValueKey('$keyPrefix-top-controls')));
+  final header = tester.getRect(find.byType(AppPageHeader));
+  final tools = tester.getRect(find.byKey(ValueKey('$keyPrefix-toolbar')));
   final palette = tester.getRect(
     find.byKey(ValueKey('$keyPrefix-color-palette')),
   );
@@ -18,10 +22,25 @@ void expectSharedDrawingLayout(
   final slider = tester.widget<AppDrawingWidthSlider>(
     find.byType(AppDrawingWidthSlider),
   );
-  expect(top.bottom, lessThanOrEqualTo(canvasRect.top));
-  expect(canvasRect.bottom, lessThanOrEqualTo(width.top));
+  expect(header.bottom, lessThanOrEqualTo(canvasRect.top));
+  expect(canvasRect.bottom, lessThanOrEqualTo(tools.top));
+  expect(tools.bottom, width.top);
   expect(width.bottom, palette.top);
-  expect(palette.width, top.width);
+  expect(palette.width, tools.width);
+  final pen = tester.getRect(find.byKey(ValueKey('$keyPrefix-pen')));
+  final eraser = tester.getRect(find.byKey(ValueKey('$keyPrefix-eraser')));
+  final undo = tester.getRect(find.byKey(ValueKey('$keyPrefix-undo')));
+  final clear = tester.getRect(find.byKey(ValueKey('$keyPrefix-clear')));
+  expect(pen.left - tools.left, closeTo(tools.right - clear.right, .001));
+  expect(eraser.left, greaterThanOrEqualTo(pen.right));
+  expect(undo.left, greaterThanOrEqualTo(eraser.right));
+  expect(clear.left - undo.right, greaterThanOrEqualTo(24));
+  expect(clear.center.dy, pen.center.dy);
+  final surface = tester.widget<ColoredBox>(find.descendant(
+    of: find.byType(AppDrawingStyleControls),
+    matching: find.byType(ColoredBox),
+  ).first);
+  expect(surface.color, AppColors.background);
   expect(width.height, 48);
   expect(canvasRect.width, closeTo(canvasRect.height, .001));
   expect(slider.canvasExtent, closeTo(canvasRect.width, .001));
